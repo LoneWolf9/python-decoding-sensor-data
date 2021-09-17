326bdd1afb443865ba953ffe0800dc1c56f7688b
LoneWolf9 <lstdon9@yahoo.com>
2021-09-17 08:12:30 -0600

    Update sensor_app.py

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -1,11 +1,14 @@
 # Runner script for all modules
-
+from load_data import load_sensor_data
 
 ##############################
 # Do not remove these two lines
 # They are needed to validate your unittest
 data = []
 print("Sensor Data App")
+
+load_sensor_data(data)
+print("Loaded records: {}".formate(len(data)))
 ##############################
 
 # Module 1 code here:

aaa2af3d3277fb9207d89f0702906f6f6dcedb6e
LoneWolf9 <lstdon9@yahoo.com>
2021-09-17 08:54:12 -0600

    Update sensor_app.py

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -22,7 +22,7 @@ print("\nHouse sensor records for area {} = {}".format(test_area, len(recs)))
 
 test_date = datetime.strptime("5/9/20", "%m/%d/%y")
 recs = house_info.get_data_by_date("id", rec_date=test_date)
-print("\n House sensor records for data: {} = {}".format(
+print("\nHouse sensor records for data: {} = {}".format(
     test_date.strftime("%m/%d/%y"), len(recs)))
 
 # Module 3 code here:

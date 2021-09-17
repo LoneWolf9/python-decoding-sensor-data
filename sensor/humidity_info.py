dd27a4998f6731d96daf59fcb820f4cd6a20b600
LoneWolf9 <lstdon9@yahoo.com>
2021-09-17 10:48:35 -0600

    Update humidity_info.py

--- sensor/humidity_info.py
+++ sensor/humidity_info.py
@@ -6,5 +6,5 @@ class HumidityData(HouseInfo):
     def _convert_data(self, data):
         recs = []
         for rec in data:
-            recs.append(float('rec' * 100))
+            recs.append(float('rec') * 100)
         return recs

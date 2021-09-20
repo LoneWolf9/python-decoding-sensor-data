b020b330cf2d7f126ff2ef26c97287a09f13b261
LoneWolf9 <lstdon9@yahoo.com>
2021-09-20 09:13:25 -0600

    Update energy_info.py

--- sensor/energy_info.py
+++ sensor/energy_info.py
@@ -28,5 +28,5 @@ class EnergyData(HouseInfo):
         return self._convert_data(recs)
 
     def calculate_energy_usage(self, data):
-        total_energy = data.sum([field * ENERGY_PER_BULB for field in data])
+        total_energy = sum([field * ENERGY_PER_BULB for field in data])
         return total_energy
